18945263e7d137180912b593adacaac8187788fc
tiemendejong <tiemendejong@live.nl>
2020-09-18 16:46:13 +0200

    final answer

--- Question 2.py
+++ Question 2.py
@@ -1,10 +1,12 @@
-response = input("How far is your destination in km")
+response = input("How far is your destination in km?")
 p = float(response)
 a = 50  #speed of car
 b = 20  #speed of biking
 c = 5   #speed of walking
+d = 5   #minutes for finding a parking spot
+e = 2   #minutes to grab your bike
 
-print("time it would take for you to arrive")
-print("car:", (p/a)*60 )
-print("car:", (p/b)*60 )
-print("car:", (p/c)*60 )
+print("time it would take for you to arrive in minutes")
+print("car:", ((p/a)*60)+d)
+print("bike:", ((p/b)*60)+e)
+print("walking:", (p/c)*60)
